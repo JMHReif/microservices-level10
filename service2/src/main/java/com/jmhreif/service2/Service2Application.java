f925c7d4f7a9290978e4ae1488750b52d737981d
Jennifer Reif <jennifer.reif@neo4j.com>
2022-10-04 15:12:24 -0500

    Initial commit

---
+++ service2/src/main/java/com/jmhreif/service2/Service2Application.java
@@ -0,0 +1,60 @@
+package com.jmhreif.service2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+@EnableRetry
+@EnableDiscoveryClient
+public class Service2Application {
+	@Value("${backend.hostname:localhost}")
+	private String hostname;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service2Application.class, args);
+	}
+
+	@Bean
+	WebClient client() {
+		return WebClient.create("http://" + hostname + ":8081");
+	}
+
+}
+
+@RestController
+@RequestMapping("/goodreads")
+@AllArgsConstructor
+class BookController {
+	private final WebClient client;
+
+	@GetMapping
+	String liveCheck() { return "Service2 is up"; }
+
+	@Retryable
+	@GetMapping("/books")
+	Flux<Book> getBooks() {
+		return client.get()
+				.uri("/db/books")
+				.retrieve()
+				.bodyToFlux(Book.class);
+	}
+}
+
+@Data
+class Book {
+	private String mongoId;
+	private String book_id;
+	private String title, format, isbn, isbn13, edition_information;
+}
---
+++ service3/src/main/java/com/jmhreif/service3/Service3Application.java
@@ -0,0 +1,63 @@
+package com.jmhreif.service3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@EnableRetry
+@EnableDiscoveryClient
+public class Service3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service3Application.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/db")
+@AllArgsConstructor
+class AuthorController {
+	private final AuthorRepository authorRepo;
+
+	@GetMapping
+	String liveCheck() { return "Service3 is up"; }
+
+	@Retryable
+	@GetMapping("/authors")
+	Flux<Author> getAuthors() { return authorRepo.findAll(); }
+
+	@Retryable
+	@GetMapping("/author/{mongoId}")
+	Mono<Author> getAuthor(@PathVariable String mongoId) { return authorRepo.findById(mongoId); }
+}
+
+interface AuthorRepository extends ReactiveCrudRepository<Author, String> {
+}
+
+@Data
+@Document
+class Author {
+	@Id
+	private String mongoId;
+	@NonNull
+	private String author_id;
+
+	private String name, average_rating, ratings_count, text_reviews_count;
+}
